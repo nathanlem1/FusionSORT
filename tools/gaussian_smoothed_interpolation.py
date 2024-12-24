ccb2444e93a27a04aeb64ae1a703ea3902fd975f
Nathanael L. Baisa <nathanaellmss@gmail.com>
2024-12-24 09:29:45 +0000

    Commenting on GSI

--- tools/gaussian_smoothed_interpolation.py
+++ tools/gaussian_smoothed_interpolation.py
@@ -12,28 +12,30 @@ from collections import defaultdict
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.gaussian_process import GaussianProcessRegressor as GPR
 
-# 线性插值
+
+# Linear interpolation
 def LinearInterpolation(input_, interval):
-    input_ = input_[np.lexsort([input_[:, 0], input_[:, 1]])]  # 按ID和帧排序
+    input_ = input_[np.lexsort([input_[:, 0], input_[:, 1]])]  # Sort by ID and frame
     output_ = input_.copy()
-    '''线性插值'''
+    '''Linear interpolation'''
     id_pre, f_pre, row_pre = -1, -1, np.zeros((10,))
     for row in input_:
         f_curr, id_curr = row[:2].astype(int)
-        if id_curr == id_pre:  # 同ID
+        if id_curr == id_pre:  # Same ID
             if f_pre + 1 < f_curr < f_pre + interval:
-                for i, f in enumerate(range(f_pre + 1, f_curr), start=1):  # 逐框插值
+                for i, f in enumerate(range(f_pre + 1, f_curr), start=1):  # Frame-by-frame interpolation
                     step = (row - row_pre) / (f_curr - f_pre) * i
                     row_new = row_pre + step
                     output_ = np.append(output_, row_new[np.newaxis, :], axis=0)
-        else:  # 不同ID
+        else:  # Different ID
             id_pre = id_curr
         row_pre = row
         f_pre = f_curr
     output_ = output_[np.lexsort([output_[:, 0], output_[:, 1]])]
     return output_
 
-# 高斯平滑
+
+# Gaussian smoothing
 def GaussianSmooth(input_, tau):
     output_ = list()
     ids = set(input_[:, 1])
@@ -59,8 +61,9 @@ def GaussianSmooth(input_, tau):
         ])
     return output_
 
+
 # GSI
-def GSInterpolation(path_in, path_out, interval, tau):
+def GSInterpolation(path_in, path_out, interval=20, tau=10):
     input_ = np.loadtxt(path_in, delimiter=',')
     li = LinearInterpolation(input_, interval)
     gsi = GaussianSmooth(li, tau)
