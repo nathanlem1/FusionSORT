e082853acdab32a9f94d4ec74e70bbff8fd65be8
Nathanael L. Baisa <nathanaellmss@gmail.com>
2024-12-27 19:07:22 +0000

    Update on fast_reid model info

--- fast_reid/fast_reid_interfece.py
+++ fast_reid/fast_reid_interfece.py
@@ -18,14 +18,13 @@ from fast_reid.fastreid.engine import DefaultTrainer, default_argument_parser, d
 
 def get_model_info(model, tsize):
 
-    stride = 64
-    img = torch.zeros((1, 3, stride, stride), device=next(model.parameters()).device)
-    flops, params = profile(deepcopy(model), inputs=(img,), verbose=False)
-    params /= 1e6
-    flops /= 1e9
-    flops *= tsize[0] * tsize[1] / stride / stride * 2  # Gflops
-    info = "Params: {:.2f}M, Gflops: {:.2f}".format(params, flops)
-    # info = "Params: {:f}M, Gflops: {:f}".format(params, flops)
+    img = torch.zeros((1, 3, tsize[0], tsize[1]), device=next(model.parameters()).device)
+    macs, params = profile(deepcopy(model), inputs=(img,), verbose=False)
+    params /= 1e6  # Number of parameters
+    macs /= 1e9   # MACs (Multiply-ACCumulate) operations
+    flops = macs * 2  # Gflops Giga FLOPs (Floating Point OPerations)
+    # info = "Params: {:f}M, Gflops: {:f}, Gmacs: {:.f}".format(params, flops, macs)
+    info = "Params: {:.4f}M, Gflops: {:.4f}, Gmacs: {:.4f}".format(params, flops, macs)
     return info
 
 
